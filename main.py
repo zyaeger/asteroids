ed775ae952fbcfd2ec2a4633ce344c3c833d27f4
zyaeger <zachyaeger@gmail.com>
2025-07-30 17:47:11 -0700

    implement player

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("Quitting Asteroids!")
                 return
+            
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for shape in drawable:
+            shape.draw(screen)
+            
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0  # seconds
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,38 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
+
+
+class Player(CircleShape):
+    def __init__(self, x: int, y: int):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w] or keys[pygame.K_s]:
+            self.move(dt)
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
