a74d7fac79bdaf59923c1944d35d8c51367448de
zyaeger <zachyaeger@gmail.com>
2025-07-30 17:19:26 -0700

    start asteroids build

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+from constants import *
+
+
+def main():
+    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
